86686e08fb1725f46045314b0739fca05b9b3797
yulirobersdev <yulirobersdev@gmail.com>
2024-06-08 17:42:01 -0400

    Add files via upload

---
+++ demo01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 17:17:38 2024
+
+@author: yipsi
+"""
+
+#This is my first and simple program on Python
+"""
+100 days on Python
+name : Variables on Python
+"""
+print("What is your name?/n")
+street = input("What´s the name of the city you grew up in?/n")
+pet = input("What is the name of your pet?/n")
+print("Your band name could be " + street + " " + pet)
+
